68c7acb1276d0e66fff6c39b2422a4f348206314
krv006 <rvkamronbek@gmail.com>
2025-02-16 01:21:44 +0500

    model

--- apps/medical/models.py
+++ apps/medical/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db.models import Model, ImageField
+from django.db.models.fields import CharField, DateTimeField, TextField, FloatField
 
-# Create your models here.
+
+class Doctor(Model):
+    name = CharField(max_length=255)
+    specialty = CharField(max_length=255)  # Mutaxassislik (masalan, kardiolog, psixolog)
+    rating = FloatField(default=0.0)  # Reyting (masalan, 4.7)
+    distance = CharField(max_length=50, blank=True, null=True)  # Masofa (masalan, 800m away)
+    image = ImageField(upload_to="doctors/", blank=True, null=True)  # Profil rasmi
+    available_time = CharField(max_length=255, blank=True, null=True)  # Ish vaqti
+
+    def __str__(self):
+        return f"Dr. {self.name} - {self.specialty}"
+
+
+class HealthArticle(Model):
+    title = CharField(max_length=255)  # Maqola sarlavhasi
+    content = TextField()  # Maqola matni
+    image = ImageField(upload_to="articles/", blank=True, null=True)  # Rasmlar
+    published_date = DateTimeField(auto_now_add=True)  # Yaratilgan sanasi
+
+    def __str__(self):
+        return self.title
+
+
+class ServiceCategory(Model):
+    name = CharField(max_length=255)  # Xizmat turi (Doctor, Pharmacy, Hospital, Ambulance)
+    icon = ImageField(upload_to="icons/", blank=True, null=True)  # Xizmat belgilari uchun rasm
+
+    def __str__(self):
+        return self.name
